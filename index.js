f5968271f98180d64359798ca06fa0c915c6353d
Yücel Kulaç <120725402+yucelkulac@users.noreply.github.com>
2024-10-25 13:35:27 +0300

    Update Server

--- index.js
+++ index.js
@@ -11,21 +11,17 @@ const io = require("socket.io")(server, {
   },
 });
 
-const port = process.env.PORT ?? 3010;
+const port = process.env.PORT ?? 3000;
 
 io.on("connection", (socket) => {
   socket.on("join", (data) => {
     const { chatId, user } = data;
     socket.join(`chat_${chatId}`);
-    // console.log(`${user.name} ${user.surname} isimli kullanıcı ${chatId} id'li sohbet odasına katıldı`);
   });
 
   socket.on("leave", (data) => {
     const { chatId, user } = data;
     socket.leave(`chat_${chatId}`);
-    // console.log(`${user.name} ${user.surname} isimli kullanıcı ${chatId} id'li sohbet odasından ayrıldı`);
-
-    // Bağlantıyı kes
     socket.disconnect();
   });
 
@@ -38,31 +34,39 @@ io.on("connection", (socket) => {
       receiver_id,
     });
   });
-});
 
-app.use(express.json());
-app.post("/message/send", async (req, res) => {
-  const { chatId, message, senderId, receiver_id, token } = req.body;
-  const send_message = await axios.post(
-    `${process.env.BASE_URL ?? "http://localhost:8000"}/api/v1/message/save`,
-    {
-      receiver_id: receiver_id,
-      content: message,
-      type: "text",
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+  // Mesaj gönderme işlemi
+  socket.on("sendMessage", async (data) => {
+    const { chatId, message, senderId, receiver_id, token } = data;
+
+    try {
+      // Mesajı kaydet
+      const send_message = await axios.post(
+        `${process.env.BASE_URL ?? "http://localhost:8000"}/api/v1/message/save`,
+        {
+          receiver_id: receiver_id,
+          content: message,
+          type: "text",
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      // Mesajı belirtilen sohbete yayınla
+      io.to(`chat_${chatId}`).emit("message", {
+        data: send_message.data,
+        sender_id: senderId,
+        message,
+        receiver_id: receiver_id,
+      });
+    } catch (error) {
+      console.error("Mesaj gönderme hatası:", error);
+      socket.emit("error", { message: "Mesaj gönderilemedi." });
     }
-  );
-  io.to(`chat_${chatId}`).emit("message", {
-    data: send_message.data,
-    sender_id: senderId,
-    message,
-    receiver_id: receiver_id,
   });
-  res.send({ status: "Mesaj yayınlandı" });
 });
 
 server.listen(port, () => {
